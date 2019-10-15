da05470dfa3ce9e76e6e64e84fd29d9dd5d6c272
Jamie Luong <jamieluong03@gmail.com>
2019-10-15 01:07:01 -0700

    updated functions within each js

---
+++ app.js
@@ -0,0 +1,6 @@
+const employee = require("./lib/employee");
+const engineer = require ("./lib/engineer");
+const intern = require ("./lib/intern");
+const manager = require ("./lib/manager");
+
+const inquire = require ("inquirer");
