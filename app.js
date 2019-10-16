57f6a0aeb8f0e123e7e58ec6c31601fd8366a4cb
Jamie Luong <jamieluong03@gmail.com>
2019-10-16 13:42:32 -0700

    modified all the questions for each job

--- app.js
+++ app.js
@@ -3,52 +3,73 @@ const employee = require("./lib/employee");
 const engineer = require ("./lib/engineer");
 const intern = require ("./lib/intern");
 const manager = require ("./lib/manager");
-const outputPath = path.resolve(_dirname, "output", "main.html");
+// const outputPath = path.resolve(_dirname, "output", "main.html");
 
 const inquirer = require ("inquirer");
 
 function promptUser(){
     return inquirer.prompt([
-    {
-        type: "input",
-        message: "What is the Manager's name?",
-        name: "name",
-        validate: function validateManager(name) {
-            return name !== '';
-        }
-    },
-    {
-        type: "input",
-        message: "What is the manager's ID?",
-        name: "id",
-        validate: function validateID(name) {
-            return name !== '';
-        }
-    },
-    {
-        type: "input",
-        message: "What is the manager's email?",
-        name: "email",
-        validate: function validateEmail(name) {
-            return name !== '';
-        }
-    },
-    {
-        type: "input",
-        message: "What is the manager's office number?",
-        name: "officenumber",
-        validate: function validateOfficeNum(name) {
-            return name !== '';
-        }
-    },
-    {
-        type: "list",
-        message: "Would you like to add another employee?",
-        choices: ["Engineer", "Intern", "No, I don't want to add another employee"],
-        name: "employee",
-    }
-    ]);
+        {
+            type: "list",
+            message: "Please add an employee",
+            choices: ["Manager", "Engineer", "Intern"],
+            name: "employee",
+        },
+    ])
+    
 };
+
+function managerQuestion(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the Manager's name?",
+            name: "name",
+            validate: function validateManager(name) {
+                return name !== '';
+            }
+        },
+        {
+            type: "input",
+            message: "What is the manager's ID?",
+            name: "id",
+            validate: function validateID(name) {
+                return name !== '';
+            }
+        },
+        {
+            type: "input",
+            message: "What is the manager's email?",
+            name: "email",
+            validate: function validateEmail(name) {
+                return name !== '';
+            }
+        },
+        {
+            type: "input",
+            message: "What is the manager's office number?",
+            name: "officenumber",
+            validate: function validateOfficeNum(name) {
+                return name !== '';
+            }
+        },
+        {
+            type: "list",
+            message: "Would you like to add another employee?",
+            choices: ["Engineer", "Intern", "No, I don't want to add another employee"],
+            name: "another",
+        }  
+    ])
+    .then(function({another}){
+        if (another === "Engineer"){
+            return engineerQuestion();
+        } else if (another === "Intern"){
+            return internQuestion();
+        } else {
+            return;
+        }
+    })
+}
 
 function internQuestion (){
     return inquirer.prompt([
@@ -80,11 +101,23 @@ function internQuestion (){
             type: "input",
             message: "What is tbe intern's school name?",
             name: "school"
+        },
+        {
+            type: "list",
+            message: "Would you like to add another employee?",
+            choices: ["Engineer", "Intern", "No, I don't want to add another employee"],
+            name: "another",
         }
     ])
-        .then(function ({name, id, email, school}){
-            console.log({name, id, email, school});
-            console.log(name, id, email, school);
+        .then(function ({name, id, email, school, another}){
+            console.log({name, id, email, school, another});
+            if (another === "Engineer"){
+                return engineerQuestion();
+            } else if (another === "Intern"){
+                return internQuestion();
+            } else {
+                return;
+            }
 
         });
 };
@@ -120,65 +153,74 @@ function engineerQuestion (){
             message: "What is the engineer's GitHub username?",
             name: "github"
         }
+        {
+            type: "list",
+            message: "Would you like to add another employee?",
+            choices: ["Engineer", "Intern", "No, I don't want to add another employee"],
+            name: "another",
+        }  
     ])
         .then(function({name, id, email, github}){
             console.log({name, id, email, github});
             console.log(name, id, email, github);
+            if (another === "Engineer"){
+                return engineerQuestion();
+            } else if (another === "Intern"){
+                return internQuestion();
+            } else {
+                return;
+            }
         });
 };
 
 
 promptUser()
-    .then(function({name, id, email, officenumber, employee}){
-        this.name = name;
-        this.email = email;
-        this.id = id;
-        this.officenumber = officenumber;
+    .then(function({employee}){
         this.employee = employee;
 
         if (employee === "Engineer"){
-            return engineerQuestion();
+            return engineerQuestion({data1});
         } else if (employee === "Intern"){
-            return internQuestion();
-        } else {
-            console.log(employee);
+            return internQuestion({data2});
+        } else if (employee === "Manager"){
+            return managerQuestion({data3});
         }
     });
 
 
 // appending data to html
 // manager card
-`<div class="card text-white mb-3" style="max-width: 18rem;">
-  <div class="card-header bg-primary">${name}${title}</div>
-  <div class="card-body">
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">${id}</li>
-    <li class="list-group-item">${email}</li>
-    <li class="list-group-item">${officenumber}</li>
-</ul>
-  </div>
-</div>`
+// `<div class="card text-white mb-3" style="max-width: 18rem;">
+//   <div class="card-header bg-primary">${name}${title}</div>
+//   <div class="card-body">
+//   <ul class="list-group list-group-flush">
+//     <li class="list-group-item">${id}</li>
+//     <li class="list-group-item">${email}</li>
+//     <li class="list-group-item">${officenumber}</li>
+// </ul>
+//   </div>
+// </div>`
 
 // engineer card
-`<div class="card text-white mb-3" style="max-width: 18rem;">
-  <div class="card-header bg-primary">${name}${title}</div>
-  <div class="card-body">
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">${id}</li>
-    <li class="list-group-item">${email}</li>
-    <li class="list-group-item">${github}</li>
-</ul>
-  </div>
-</div>`
+// `<div class="card text-white mb-3" style="max-width: 18rem;">
+//   <div class="card-header bg-primary">${name}${title}</div>
+//   <div class="card-body">
+//   <ul class="list-group list-group-flush">
+//     <li class="list-group-item">${id}</li>
+//     <li class="list-group-item">${email}</li>
+//     <li class="list-group-item">${github}</li>
+// </ul>
+//   </div>
+// </div>`
 
 // intern card
-`<div class="card text-white mb-3" style="max-width: 18rem;">
-  <div class="card-header bg-primary">${name}${title}</div>
-  <div class="card-body">
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">${id}</li>
-    <li class="list-group-item">${email}</li>
-    <li class="list-group-item">${school}</li>
-</ul>
-  </div>
-</div>`
+// `<div class="card text-white mb-3" style="max-width: 18rem;">
+//   <div class="card-header bg-primary">${name}${title}</div>
+//   <div class="card-body">
+//   <ul class="list-group list-group-flush">
+//     <li class="list-group-item">${id}</li>
+//     <li class="list-group-item">${email}</li>
+//     <li class="list-group-item">${school}</li>
+// </ul>
+//   </div>
+// </div>`
